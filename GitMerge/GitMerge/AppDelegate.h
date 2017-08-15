7404a2f06a0bd99e6c23fe335df371675b5d9862
Jessie <huhanqun_001@126.com>
2017-08-15 09:05:19 +0800

    add - project

---
+++ GitMerge/GitMerge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitMerge
+//
+//  Created by Jessie on 2017/8/15.
+//  Copyright © 2017年 Jessie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
