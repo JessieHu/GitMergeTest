7ff34c0c290197d6c17fe909bdbb286b90e2402b
Jessie <huhanqun_001@126.com>
2017-08-15 09:13:07 +0800

    update - 首页

--- GitMerge/GitMerge/ViewController.h
+++ GitMerge/GitMerge/ViewController.h
@@ -8,6 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
+/// 首页.
 @interface ViewController : UIViewController
 
 
--- GitMerge/GitMerge/ViewController.m
+++ GitMerge/GitMerge/ViewController.m
@@ -16,13 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSLog(@"随便写写");
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 
